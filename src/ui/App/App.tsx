91b4cc3f63cd02f87352aac35fa7b6b56a9dce50
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-03-13 23:08:22 -0400

    initial boilerplate

---
+++ src/ui/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div``
+
+
+const App = () => {
+    return (
+        <AppWrapper>
+            <p>Hi</p>
+        </AppWrapper>
+
+
+    )
+}
+
+export default App
