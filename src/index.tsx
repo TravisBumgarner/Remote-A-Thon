cf6b41bcda17d99a90b6bc4092104caf59f4d01b
Travis Bumgarner <travis.bumgarner@gmail.com>
2020-03-20 17:34:37 -0400

    Added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from '@sentry/browser'
 
+import { Error } from './App/components'
 import App from './App'
 
-const renderApp = () => {
-    ReactDOM.render(
-        <App />,
-        document.getElementById('root')
-    )
+Sentry.init({
+    dsn: 'https://99cf7d1bca5843b4848a969de4aaf975@sentry.io/5169383'
+})
+
+class SentryWrapper extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            error: null
+        }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({ error })
+        Sentry.withScope(scope => {
+            Object.keys(errorInfo).forEach(key => {
+                scope.setExtra(key, errorInfo[key])
+            })
+            Sentry.captureException(error)
+        })
+    }
+
+    render() {
+        if (this.state.error) {
+            return <Error />
+        } else {
+            return this.props.children
+        }
+    }
 }
-renderApp()
+
+ReactDOM.render(
+    <SentryWrapper>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </SentryWrapper>,
+    document.getElementById('root')
+)
